800fec405589e4e091181beef4fc09bbed3d032e
Rob Green <rob@200sx.org.uk>
2017-08-07 11:05:54 +0100

    commit prior to requirejs integration

--- content/js/magnet_link_checker.js
+++ content/js/magnet_link_checker.js
@@ -4,6 +4,11 @@
 
 var port = chrome.runtime.connect({name: 'magnetCheck'});
 
+port.onMessage.addListener(function(response) {
+    //console.log(response);
+});
+
 if ($("a[href^='magnet:']").length) {
+    //console.log('found magnets, posting success message.');
     port.postMessage({magnets: true});
 }
--- eventPage.js
+++ eventPage.js
@@ -5,7 +5,9 @@ chrome.runtime.onConnect.addListener(function(port) {
                 if (request.magnets) {
                     chrome.tabs.insertCSS(null, {file: 'content/css/bootstrap.micro.min.css'}, function() {
                         chrome.tabs.executeScript(null, {file: 'content/js/bootstrap.micro.min.js'}, function() {
-                            chrome.tabs.executeScript(null, {file: 'content/js/content_script.js'});
+                            chrome.tabs.executeScript(null, {file: 'content/js/content_script.js'}, function() {
+                                //port.postMessage('loaded new content-script');
+                            });
                         });
                     });
                 }
